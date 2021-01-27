68df6650bd8c730ecd633f9554b5ea2ff72ba42b
Legalpedia <support@legalpediaonline.com>
2021-01-27 16:30:35 +0100

    authentication corrected

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,18 +1,20 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
-
 class AuthService {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-  
+
   Stream<String> get onAuthStateChanged =>
-      _firebaseAuth.onAuthStateChanged.map(
-      (FirebaseUser user) => user?.uid,
-  );
+      _firebaseAuth.authStateChanges().map(
+            (User user) => user?.uid,
+      );
 
   // Email & Password Sign Up
+  Future<String> createUserWithEmailAndPassword(String email, String password,
+      String name) async {
+    final currentUser = await _firebaseAuth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
 
-Future<String> createUserWithEmailAndPassword(String email, String password, String name) async {
-  final currentUser = await _firebaseAuth.createUserWithEmailAndPassword(email: email, password: password,);
-}
-    )
 }
