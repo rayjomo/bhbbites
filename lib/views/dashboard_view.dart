29fe195eebd81c77e21048a10bab08962d23b5da
Legalpedia <support@legalpediaonline.com>
2021-02-08 22:37:14 +0100

    authentication corrected

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -62,18 +62,64 @@ class BannerWidgetArea extends StatelessWidget {
     var screenHeight = MediaQuery.of(context).size.height;
     var screenWidth = MediaQuery.of(context).size.width;
 
-    PageController controller = PageController(initialPage: 1);
+    PageController controller = PageController(viewportFraction: 0.8, initialPage: 1);
 
     List<Widget> banners = new List<Widget>();
 
     for(int x = 0; x < bannerItems.length; x++){
-      var bannerView = Container(
-        child: Stack(
-          fit: StackFit.expand,
-          children: <Widget>[
-            Image.asset(bannerImage[x], fit: BoxFit.cover,)
+      var bannerView = Padding(
+        padding: EdgeInsets.all(10.0),
+            child: Container(
+              child: Stack(
+                fit: StackFit.expand,
+                children: <Widget>[
+
+                  //Add dropshadow
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black,
+                          offset: Offset(2.0, 2.0),
+                          blurRadius: 5.0,
+                            spreadRadius: 1.0
+                        )
+                      ]
+                    )
+                  ),
+
+                // Add curved edges
+                  ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular((20.0))),
+                    child: Image.asset(
+                      bannerImage[x],
+                      fit: BoxFit.cover
+                    ),
+                  ),
+
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [Colors.transparent,Colors.black])
+                    ),
+                  ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(bannerItems[x],
+                        style: TextStyle(fontSize: 20.0, color: Colors.white),),
+                    Text("More than 40% Off", style: TextStyle(fontSize: 18.0, color: Colors.white),)
+                  ],
+                )
+
         ],
-        ),
+      ),
+    ),
       );
       banners.add(bannerView);
     }
